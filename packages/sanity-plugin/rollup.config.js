f159242d63e5a4fee5d17d5c5b72c7f35a3552dd
Joseph Thomas <joseph@good-idea.studio>
2019-06-16 14:55:32 -0700

    chore(sanity-plugin): update rollup config

--- packages/sanity-plugin/rollup.config.js
+++ packages/sanity-plugin/rollup.config.js
@@ -3,15 +3,23 @@ import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.ts',
-  output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
-  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {}), 'lodash-es/uniqueId'],
+  input: ['src/index.ts', 'src/schema.tsx'],
+  output: {
+    dir: './lib',
+    format: 'es'
+    // file: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }]
+  },
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+    'lodash-es/uniqueId'
+  ],
   plugins: [
     // tsConfigPaths({
     //   // tsConfigDirectory: process.cwd(),
     // }),
     typescript({
-      typescript: require('typescript'),
-    }),
-  ],
+      typescript: require('typescript')
+    })
+  ]
 }
