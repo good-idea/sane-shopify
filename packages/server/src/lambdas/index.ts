35b83d61e6e8858cff5fe7b2ff2ccfe4531f0248
Joseph Thomas <joseph@good-idea.studio>
2019-08-09 11:20:34 -0700

    feat(server): require sanity auth token for webhooks

--- packages/server/src/lambdas/index.ts
+++ packages/server/src/lambdas/index.ts
@@ -25,6 +25,10 @@ const createEventHandlers = (resolve, reject) => ({
 })
 
 export const createLambdas = (config: SaneShopifyConfig): SaneLambdas => {
+  if (!config.sanity.authToken)
+    throw new Error(
+      'You must provide a Sanity client auth token to use the Shopify webhooks. If you only want to use the GraphQL Lambda, use `createGraphQLHandler` instead. To find your Auth token, run `sanity debug --secrets`'
+    )
   const client = createSyncingClient(config)
 
   const graphql = createGraphQLHandler(config)
