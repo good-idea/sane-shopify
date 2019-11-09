d091db93e6d326e765cd32cfe9cf346bdf38c136
Joseph Thomas <joseph@good-idea.studio>
2019-11-09 15:04:42 -0800

    chore(repo): update all dependencies

--- packages/sync-utils/rollup.config.js
+++ packages/sync-utils/rollup.config.js
@@ -4,16 +4,19 @@ import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
-  output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' }
+  ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependnencies || {}),
     'rxjs/operators',
-    'lodash-es/isMatch',
+    'lodash-es/isMatch'
   ],
   plugins: [
     typescript({
-      typescript: require('typescript'),
-    }),
-  ],
+      typescript: require('typescript')
+    })
+  ]
 }
