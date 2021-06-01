cd3cea8a9917b854bd505c0a103c869ea1671a25
Joseph Thomas <joseph@good-idea.studio>
2021-05-31 20:05:59 -0700

    fix(sync-utils): fix prettier errors

--- packages/sync-utils/src/sanity/fetchRelatedDocs.ts
+++ packages/sync-utils/src/sanity/fetchRelatedDocs.ts
@@ -12,37 +12,34 @@ interface CacheResults {
   idsNotInCache: string[]
 }
 
-export const createFetchRelatedDocs = (
-  client: SanityClient,
-  cache: SanityCache
-): SanityUtils['fetchRelatedDocs'] => async (
-  relatedNodes: ShopifyItem[]
-): Promise<RelatedPairPartial[]> => {
-  const relatedIds = relatedNodes.map((r) => r.id)
-  const { cachedDocs, idsNotInCache } = relatedIds
-    .map((id) => ({
-      cachedItem: cache.getByShopifyId(id),
-      id,
-    }))
-    .reduce<CacheResults>(
-      (acc, current) => {
-        if (current.cachedItem === null) {
+export const createFetchRelatedDocs =
+  (client: SanityClient, cache: SanityCache): SanityUtils['fetchRelatedDocs'] =>
+  async (relatedNodes: ShopifyItem[]): Promise<RelatedPairPartial[]> => {
+    const relatedIds = relatedNodes.map((r) => r.id)
+    const { cachedDocs, idsNotInCache } = relatedIds
+      .map((id) => ({
+        cachedItem: cache.getByShopifyId(id),
+        id,
+      }))
+      .reduce<CacheResults>(
+        (acc, current) => {
+          if (current.cachedItem === null) {
+            return {
+              ...acc,
+              idsNotInCache: [...acc.idsNotInCache, current.id],
+            }
+          }
           return {
             ...acc,
-            idsNotInCache: [...acc.idsNotInCache, current.id],
+            cachedDocs: [...acc.cachedDocs, current.cachedItem],
           }
-        }
-        return {
-          ...acc,
-          cachedDocs: [...acc.cachedDocs, current.cachedItem],
-        }
-      },
-      { cachedDocs: [], idsNotInCache: [] }
-    )
+        },
+        { cachedDocs: [], idsNotInCache: [] }
+      )
 
-  const fetched = idsNotInCache.length
-    ? await client.fetch<SanityShopifyDocument[]>(
-        `
+    const fetched = idsNotInCache.length
+      ? await client.fetch<SanityShopifyDocument[]>(
+          `
     *[shopifyId in $relatedIds && defined(archived) && archived != true && !(_id in path('drafts.**'))]{
       ...,
       products[]->{
@@ -56,15 +53,15 @@ export const createFetchRelatedDocs = (
       "collectionRefs": collections[],
       "productRefs": products[],
      }`,
-        { relatedIds: idsNotInCache }
-      )
-    : []
-  fetched.forEach((doc) => cache.set(doc))
-  const relatedDocs = [...cachedDocs, ...fetched]
-  const pairs = relatedNodes.map((shopifyNode) => ({
-    shopifyNode,
-    sanityDocument:
-      relatedDocs.find((d) => d.shopifyId === shopifyNode.id) || null,
-  }))
-  return pairs
-}
+          { relatedIds: idsNotInCache }
+        )
+      : []
+    fetched.forEach((doc) => cache.set(doc))
+    const relatedDocs = [...cachedDocs, ...fetched]
+    const pairs = relatedNodes.map((shopifyNode) => ({
+      shopifyNode,
+      sanityDocument:
+        relatedDocs.find((d) => d.shopifyId === shopifyNode.id) || null,
+    }))
+    return pairs
+  }
